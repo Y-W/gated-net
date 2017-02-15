59eddc4feff59e4c0e531cde32eff2076cf1d6da
Yijie Wang <wyijie93@gmail.com>
2017-02-15 16:04:42 -0500

    YW

--- slim_model/preprocessing/preprocessing_factory.py
+++ slim_model/preprocessing/preprocessing_factory.py
@@ -20,10 +20,10 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-from preprocessing import cifarnet_preprocessing
-from preprocessing import inception_preprocessing
-from preprocessing import lenet_preprocessing
-from preprocessing import vgg_preprocessing
+import cifarnet_preprocessing
+import inception_preprocessing
+import lenet_preprocessing
+import vgg_preprocessing
 
 slim = tf.contrib.slim
 
