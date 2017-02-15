6c5f107aa5d9ff8a1033a2b4e81264ed87e190d4
Yijie Wang <wyijie93@gmail.com>
2017-02-15 16:38:42 -0500

    YW

--- slim_model/datasets/imagenet.py
+++ slim_model/datasets/imagenet.py
@@ -52,8 +52,6 @@ _ITEMS_TO_DESCRIPTIONS = {
     'image': 'A color image of varying height and width.',
     'label': 'The label id of the image, integer between 0 and 999',
     'label_text': 'The text of the label.',
-    'object/bbox': 'A list of bounding boxes.',
-    'object/label': 'A list of labels, one per each object.',
 }
 
 _NUM_CLASSES = 1001
@@ -152,25 +150,12 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
           [], dtype=tf.int64, default_value=-1),
       'image/class/text': tf.FixedLenFeature(
           [], dtype=tf.string, default_value=''),
-      'image/object/bbox/xmin': tf.VarLenFeature(
-          dtype=tf.float32),
-      'image/object/bbox/ymin': tf.VarLenFeature(
-          dtype=tf.float32),
-      'image/object/bbox/xmax': tf.VarLenFeature(
-          dtype=tf.float32),
-      'image/object/bbox/ymax': tf.VarLenFeature(
-          dtype=tf.float32),
-      'image/object/class/label': tf.VarLenFeature(
-          dtype=tf.int64),
   }
 
   items_to_handlers = {
       'image': slim.tfexample_decoder.Image('image/encoded', 'image/format'),
       'label': slim.tfexample_decoder.Tensor('image/class/label'),
       'label_text': slim.tfexample_decoder.Tensor('image/class/text'),
-      'object/bbox': slim.tfexample_decoder.BoundingBox(
-          ['ymin', 'xmin', 'ymax', 'xmax'], 'image/object/bbox/'),
-      'object/label': slim.tfexample_decoder.Tensor('image/object/class/label'),
   }
 
   decoder = slim.tfexample_decoder.TFExampleDecoder(
