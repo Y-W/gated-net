debc5df0c791882ef16c114eb0f45d0f2b817657
Yijie Wang <wyijie93@gmail.com>
2017-04-06 13:37:51 -0400

    YW

--- slim_model/resnet_v1_hard_branch.py
+++ slim_model/resnet_v1_hard_branch.py
@@ -56,6 +56,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import functools
+
 import tensorflow as tf
 from tensorflow.python.framework import ops
 
