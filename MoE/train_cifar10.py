bddb4f2150572f78e255279449ec4e88f51bd781
Yijie Wang <wyijie93@gmail.com>
2017-05-10 16:12:35 -0400

    YW

--- MoE/train_cifar10.py
+++ MoE/train_cifar10.py
@@ -128,10 +128,12 @@ def prepare_net(batch_queue, num_samples):
     tf.summary.scalar('mean_preact_loss', moving_stats.average(mean_preact_loss))
     tf.summary.scalar('learning_rate', learning_rate)
     tf.summary.scalar('slope_rate', slope_rate)
-    # if 'branch_result' in end_points:
-    #     tf.summary.histogram('branch_result', end_points['branch_result'])
-    #     tf.summary.histogram('branch_preact', end_points['branch_preact'])
-    #     tf.summary.histogram('branch_soft', tf.nn.softmax(end_points['branch_preact'], dim=1))
+    if 'branch_result' in end_points:
+        tf.summary.histogram('branch_result', end_points['branch_result'])
+        tf.summary.histogram('branch_preact', end_points['branch_preact'])
+        tf.summary.histogram('branch_soft', tf.nn.softmax(end_points['branch_preact'], dim=1))
+        tf.summary.histogram('branch_noise', end_points['branch_noise'])
+        tf.summary.histogram('branch_noise_real', tf.nn.softplus(end_points['branch_noise']))
     summary_op = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES), name='summary_op')
     
     update_op = tf.group(*tf.get_collection(tf.GraphKeys.UPDATE_OPS))
